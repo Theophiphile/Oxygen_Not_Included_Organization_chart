b53b6a5957c059b6515b64d4b0d321ced07133c2
Theo <theophiphile@outlook.fr>
2024-02-25 19:11:11 +0100

    Le code est un peu plus joli

---
+++ script/main.js
@@ -0,0 +1,95 @@
+let DbList;
+
+function lookInDb(key) {
+    var ret = [];
+    if ('High' in db.elements[key]) {
+        ret.push(...Object.keys(db.elements[key].High.elements));
+    }
+    if ('Low' in db.elements[key]) {
+        ret.push(...Object.keys(db.elements[key].Low.elements));
+    }
+    for (const [k, v] of Object.entries(db.elements)) {
+        if ('High' in v && key in v.High.elements) {
+            ret.push(k);
+        }
+        if ('Low' in v && key in v.Low.elements) {
+            ret.push(k);
+        }
+    }
+    console.log([...new Set(ret)]);
+    while (DbList.firstChild) {
+        DbList.firstChild.remove();
+    }
+    for (const e of [...new Set(ret)]) {
+        elem = document.getElementById(e).cloneNode(true);
+        elem.removeAttribute('id');
+        DbList.insertAdjacentElement("afterbegin", elem);
+    }
+}
+
+function onMouseMoveSnapGrid(c, offsetX, offsetY, event) {
+    c.style.left = ((event.pageX - offsetX) / 20).toFixed() * 20 + 'px';
+    c.style.top = ((event.pageY - offsetY) / 20).toFixed() * 20 + 'px';
+}
+
+function onmousedownGeneric(c, event) {
+    offsetX = event.pageX - c.getBoundingClientRect().left;
+    offsetY = event.pageY - c.getBoundingClientRect().top;
+    lookInDb(c.getAttribute("element"));
+    omm = event => onMouseMoveSnapGrid(c, offsetX, offsetY, event);
+    omm(event);
+    document.addEventListener('mousemove', omm);
+    document.onmouseup = function (event) {
+        document.removeEventListener('mousemove', omm);
+        document.onmouseup = null;
+        if (((event.pageX - offsetX) / 20).toFixed() * 20 < 140) {
+            c.remove();
+        }
+    };
+}
+function onmousedownForElementsList(figure, event) {
+    let c = figure.cloneNode(true);
+    c.removeAttribute('id');
+    c.style.position = 'absolute';
+    document.body.append(c);
+    c.onmousedown = event => onmousedownGeneric(c, event);
+    offsetX = event.pageX - figure.getBoundingClientRect().left;
+    offsetY = event.pageY - figure.getBoundingClientRect().top;
+    onMouseMoveSnapGrid(c, offsetX, offsetY, event);
+    onmousedownGeneric(c, event);
+}
+
+window.addEventListener('load', function () {
+    let elementsList = document.createElement('div');
+    elementsList.id = "ElementsList";
+    DbList = document.createElement('div');
+    elementsList.style.width = '132px';
+    elementsList.style.height = '500px';
+    elementsList.style.overflow = 'auto';
+    DbList.style.width = '132px';
+    DbList.style.height = '500px';
+    DbList.style.overflow = 'auto';
+    DbList.style.position = 'absolute';
+    DbList.style.left = window.innerWidth - 140 + 'px';
+    DbList.style.top = 0;
+    for (const [key, value] of Object.entries(db.elements)) {
+        let figure = document.createElement("figure");
+        figure.className = "elements";
+        figure.id = key;
+        figure.setAttribute("element", key);
+        figure.onmousedown = event => onmousedownForElementsList(figure, event);
+        let image = document.createElement('img');
+        console.log(img.elements);
+        image.setAttribute('src', 'images/elements/' + img.elements[key] + '.png');
+        image.draggable = false;
+        let caption = document.createElement("figcaption");
+        caption.insertAdjacentHTML('afterbegin', key);
+        figure.appendChild(image);
+        figure.appendChild(caption);
+        console.log(figure);
+        elementsList.appendChild(figure);
+    }
+    document.body.appendChild(elementsList);
+    document.body.appendChild(DbList);
+
+});
