f7911d8abd053f29ad887aa9e474e7ff382f9ffe
Theo <theophiphile@outlook.fr>
2024-02-26 18:37:26 +0100

    Responsive

--- script/main.js
+++ script/main.js
@@ -34,7 +34,6 @@ function onMouseMoveSnapGrid(c, offsetX, offsetY, event) {
 }
 
 function onmousedownGeneric(c, event, lookdb = true) {
-    overlaysStartArrows.style.visibility = 'hidden';
     offsetX = event.pageX - c.getBoundingClientRect().left;
     offsetY = event.pageY - c.getBoundingClientRect().top;
     if (lookdb) {
@@ -73,17 +72,17 @@ function initElementsList() {
     elementsList.id = "ElementsList";
 
     for (const [key, value] of Object.entries(db.elements)) {
-        let figure = document.createElement("figure");
+        let figure = document.createElement("div");
         figure.className = "elements";
         figure.id = key;
         figure.setAttribute("element", key);
         figure.onmousedown = event => onmousedownForElementsList(figure, event);
         let image = document.createElement('img');
-        console.log(img.elements);
         image.setAttribute('src', 'images/elements/' + img.elements[key] + '.png');
         image.draggable = false;
-        let caption = document.createElement("figcaption");
-        caption.insertAdjacentHTML('afterbegin', key);
+        let caption = document.createElement("div");
+        caption.className = "textElement";
+        caption.appendChild(document.createTextNode(key));
         figure.appendChild(image);
         figure.appendChild(caption);
         console.log(figure);
@@ -94,11 +93,9 @@ function initElementsList() {
 
 window.addEventListener('load', function () {
     initElementsList();
-    initOverlaysStartArrows();
     DbList = document.createElement('div');
     DbList.style.width = '137px';
     DbList.style.overflow = 'scroll';
-    DbList.style.position = 'absolute';
     DbList.style.left = window.innerWidth - 140 + 'px';
     DbList.style.top = 0;
     document.body.appendChild(DbList);
