b4aad1bdd233950e75163fad190cf1dececf76f3
Theo <theophiphile@outlook.fr>
2024-02-27 14:10:29 +0100

    Menu qui se construit tout seul :o

--- script/main.js
+++ script/main.js
@@ -1,5 +1,6 @@
 let DbList;
 
+
 function arrowClickButton() {
     let butt = document.getElementById('buttonArrow');
     if (butt.style.backgroundColor == "") {
@@ -78,53 +79,65 @@ function onmousedownForElementsList(figure, event, lookdb = true) {
     onmousedownGeneric(c, event, lookdb);
 }
 
-function initElementsList() {
-    let elementsList = document.getElementById("ElementsList");
-    for (const [key, value] of Object.entries(img.elements)) {
-        let figure = document.createElement("div");
-        figure.className = "elements";
-        figure.id = key;
-        figure.setAttribute("element", key);
-        figure.onmousedown = event => onmousedownForElementsList(figure, event);
-        let image = document.createElement('img');
-        image.setAttribute('src', 'images/elements/' + value + '.png');
-        image.draggable = false;
-        let caption = document.createElement("div");
-        caption.className = "textElement";
-        caption.appendChild(document.createTextNode(key));
-        figure.appendChild(image);
-        figure.appendChild(caption);
-        console.log(figure);
-        elementsList.appendChild(figure);
+function createMenu() {
+    let stylesheet = document.createElement("style");
+    stylesheet.setAttribute("type", "text/css");
+    document.head.appendChild(stylesheet);
+    let styles = stylesheet.sheet;
+    for (const [key, color] of Object.entries(img.colors)) {
+        [r, g, b] = color.split(' ');
+        styles.insertRule('.' + key + 's{background-color:rgb(' + r * 1.7 + ',' + g * 1.7 + ',' + b * 1.7 + ');img{background-color:rgb(' + r * 1.3 + ',' + g * 1.3 + ',' + b * 1.3 + ')}}', styles.cssRules.length);
+        styles.insertRule('#' + key + 'sButton{background-color:rgb(' + r + ',' + g + ',' + b + ');}', styles.cssRules.length);
+        styles.insertRule('#' + key + 'sList{background-color:rgb(' + r + ',' + g + ',' + b + ');}', styles.cssRules.length);
     }
-    let crittersList = document.getElementById("CrittersList");
-    for (const [key, value] of Object.entries(img.critters)) {
-        let figure = document.createElement("div");
-        figure.className = "critters";
-        figure.id = key;
-        figure.setAttribute("critter", key);
-        figure.onmousedown = event => onmousedownForElementsList(figure, event);
-        let image = document.createElement('img');
-        image.setAttribute('src', 'images/critters/' + value + '.png');
-        image.draggable = false;
-        let caption = document.createElement("div");
-        caption.className = "textElement";
-        caption.appendChild(document.createTextNode(key));
-        figure.appendChild(image);
-        figure.appendChild(caption);
-        console.log(figure);
-        crittersList.appendChild(figure);
+    console.log(styles);
+    let menu = document.getElementById('leftMenu');
+    let middle = document.getElementById('middle');
+    for (const [str, entries] of Object.entries(img)) {
+        if (str == 'colors')
+            continue;
+
+        let list = document.createElement("div");
+        list.id = str + "List";
+        list.classList.add('dominoList');
+        
+        let butt = document.createElement("div");
+        butt.classList.add('leftMenuButton')
+        butt.id = str + 'Button';
+        butt.onclick = () => HideAllListsBut(list.id);
+        let i = document.createElement("img");
+        i.src = 'images/' + str + '/' + Object.entries(img[str])[0][1] + '.png';
+        butt.appendChild(i);
+        menu.appendChild(butt);
+
+        for (const [key, value] of Object.entries(img[str])) {
+            let figure = document.createElement("div");
+            figure.classList.add('domino', str);
+            figure.id = key;
+            figure.setAttribute(str.slice(0, -1), key);
+            figure.onmousedown = event => onmousedownForElementsList(figure, event);
+            let image = document.createElement('img');
+            image.setAttribute('src', 'images/' + str + '/' + value + '.png');
+            image.draggable = false;
+            let caption = document.createElement("div");
+            caption.className = "textElement";
+            caption.appendChild(document.createTextNode(key));
+            figure.appendChild(image);
+            figure.appendChild(caption);
+            list.appendChild(figure);
+        }
+        middle.appendChild(list);
     }
 }
 
 function HideAllListsBut(visible) {
-    for (hide of document.getElementsByClassName('ChoicesList')) 
+    for (hide of document.getElementsByClassName('dominoList'))
         hide.style.display = hide.id == visible ? 'flex' : 'none';
 }
 
 
 window.addEventListener('load', function () {
-    initElementsList();
+    createMenu();
     HideAllListsBut('ElementsList');
     DbList = document.createElement('div');
     DbList.style.width = '137px';
